c63f127968d9410976bd1ad9ddb191592a40cc27
jesse zenon <jesse.zenon@gmail.com>
2022-01-09 07:59:45 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(`${name}`);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(`${name}`);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    let newArray = [...cats];
+    newArray.push(name);
+    return newArray;
+}
+
+function prependCat(name) {
+    let newArray1 = [...cats];
+    newArray1.unshift(name);
+    return newArray1;
+}
+
+function removeLastCat(name) {
+    let newArray2 = [...cats];
+    newArray2.pop(name);
+    return newArray2;
+}
+
+function removeFirstCat(name) {
+    let newArray3 = [...cats];
+    newArray3.shift(name);
+    return newArray3;
+}
